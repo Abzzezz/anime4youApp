c06b87628f915f5b1eb060866f68831d4df577de
Yugata <48448458+Abzzezz@users.noreply.github.com>
2020-10-13 16:41:25 +0200

    clean

--- app/build/generated/source/buildConfig/debug/net/bplaced/abzzezz/animeapp/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/net/bplaced/abzzezz/animeapp/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "net.bplaced.abzzezz.animeapp";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 15;
-  public static final String VERSION_NAME = "12.0.2";
+  public static final int VERSION_CODE = 16;
+  public static final String VERSION_NAME = "12.0.3";
 }
